3acdf8a3da06a867015d2889996976ee54bbcbfc
jisoo <jisoosong97@gmail.com>
2023-03-23 16:40:33 +0900

    update bfs

--- Tree/BinarySearchTree.js
+++ Tree/BinarySearchTree.js
@@ -81,15 +81,3 @@ class BinarySearchTree {
         return data;
     }
 }
-
-const tree = new BinarySearchTree();
-
-tree.insert(10);
-tree.insert(6);
-tree.insert(15);
-tree.insert(3);
-tree.insert(8);
-tree.insert(20);
-
-const result = tree.bfs();
-console.log(result);
